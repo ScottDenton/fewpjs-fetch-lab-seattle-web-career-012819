c8d5acae0df22d36b9eae41d409fd691b01bd3e9
ScottDenton <scottyd_14@hotmail.com>
2019-03-12 17:29:27 -0700

    Done.

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
-function fetchBooks() {
-}
-
 function renderBooks(json) {
-  const main = document.querySelector('main')
+  const main = document.querySelector('body')
   json.forEach(book => {
-    const h2 = document.createElement('h2')
+    const h2 = document.createElement('h2');
     h2.innerHTML = `<h2>${book.name}</h2>`
     main.appendChild(h2)
   })
 }
 
+function fetchBooks() {
+  fetch('https://anapioficeandfire.com/api/books')
+  .then(response => response.json())
+  .then(json => renderBooks(json))
+}
+
 document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks()
+  fetchBooks();
 })
